4f474714e377a83a6ea50f9f1adbde92186d04aa
Will Boyd <wbinventor@gmail.com>
2014-03-03 23:52:57 -0500

    Restored doppler.py to use 100000 neutrons per batch by default

--- sample-input/doppler.py
+++ sample-input/doppler.py
@@ -19,7 +19,7 @@ from pinspec.log import *
 
 # Set main simulation params
 num_batches = 10
-num_neutrons_per_batch = 10000
+num_neutrons_per_batch = 100000
 num_threads = 4
 setOutputDirectory('doppler');
 py_setlevel('INFO')
